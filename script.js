65f90878f9f87a350e817fd184ca56d893dacdb8
Michael Ronald <ronaldmichael918@gmail.com>
2020-11-29 23:13:15 -0300

    Projeto completo funcionando

---
+++ script.js
@@ -0,0 +1,25 @@
+function envioEmail() {
+    var email = document.getElementById("email").value;
+    // Validando email
+    var isArroba = false;
+    var isPontoCom = false;
+    for (var i = 0; i < email.length; i++) {
+        if (email[i] == "@") {
+            isArroba = true;
+            i = email.length + 1;
+        }
+    }
+    for (var i = 0; i < email.length; i++) {
+        if (email[i] == "." && email[i+1] == "c" && email[i+2] == "o" && email[i+3] == "m") {
+            isPontoCom = true;
+            i = email.length + 1;
+        }
+    }
+    if (!isArroba || !isPontoCom) {
+        alert("Email inválido!");
+    } else {
+        alert("E-mail enviado com sucesso! Muito obrigado, entraremos em contato!");
+        var bemVindo = document.getElementById("msg");
+        bemVindo.innerText = "Seja bem-vindo, " + email + "!";
+    }
+}
